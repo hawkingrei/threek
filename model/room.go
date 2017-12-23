b96c4d98fae791d4cbc21e88525a21b7d2027719
真人 <weizhen.wang@duitang.com>
2017-12-23 17:45:15 +0800

    model: add room

--- model/room.go
+++ model/room.go
@@ -1,31 +1,9 @@
 package model
 
 type Room struct {
+	Rid      int64 `gorm:"AUTO_INCREMENT;primary_key"`
 	Lord     User
-	Loyalist []User
-	Quisling []User
-	Thief    []User
-}
-
-var roomMap map[int]Room
-
-func GetRoomMap() map[int]Room {
-	tmp := Room{}
-	tmp.Loyalist = make([]User, 1)
-	tmp.Quisling = make([]User, 1)
-	tmp.Thief = make([]User, 1)
-	roomMap[4] = tmp
-
-	tmp = Room{}
-	tmp.Loyalist = make([]User, 1)
-	tmp.Quisling = make([]User, 1)
-	tmp.Thief = make([]User, 2)
-	roomMap[5] = tmp
-
-	tmp = Room{}
-	tmp.Loyalist = make([]User, 1)
-	tmp.Quisling = make([]User, 1)
-	tmp.Thief = make([]User, 3)
-	roomMap[6] = tmp
-	return roomMap
+	Loyalist User
+	Quisling User
+	Thief    User
 }
